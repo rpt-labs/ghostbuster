4cacd53bf9050ad0674c12dfc9b5ed9087e8eb43
michellelockett <shelldog42@gmail.com>
2018-10-10 22:07:26 -0700

    update app.js paths to reflect file structure change

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const asyncMiddleware = require('./helpers/asyncMiddleware');
+const asyncMiddleware = require('./server/helpers/asyncMiddleware');
 const port = process.env.PORT || 1234;
 
 //controllers
-const sprintsController = require('./controllers/sprintsController');
-const contributionsController = require('./controllers/contributionsController');
-const teamsController = require('./controllers/teamsController');
+const sprintsController = require('./server/controllers/sprintsController');
+const contributionsController = require('./server/controllers/contributionsController');
+const teamsController = require('./server/controllers/teamsController');
 
 //check sprints for pairing phase
 app.get('/ghostbuster/sprints/:sprintNames', cors(), asyncMiddleware(sprintsController));
