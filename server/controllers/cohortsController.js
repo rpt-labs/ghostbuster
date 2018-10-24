7a56b1aeb0435baac7461ba2b4da7d4e21033427
michellelockett <shelldog42@gmail.com>
2018-10-24 13:07:11 -0700

    adds routes for cohort & stuent information

---
+++ server/controllers/cohortsController.js
@@ -0,0 +1,41 @@
+const cohorts = require('../config/cohorts');
+const cohortNames = Object.keys(cohorts);
+
+exports.getCohorts = (req, res) => {
+  const sprintCohorts = cohortNames
+    .filter(cohort => cohorts[cohort].phase === 'sprint')
+    .map(cohort => cohort.toUpperCase());
+  const projectCohorts = cohortNames
+    .filter(cohort => !sprintCohorts.includes(cohort))
+    .map(cohort => cohort.toUpperCase());
+  res.send({ sprintCohorts, projectCohorts });
+};
+
+exports.createCohort = (req, res) => {
+  res.send("add functionality to create a cohort");
+};
+
+exports.updateCohort = (req, res) => {
+  res.send("add functionality to update a cohort");
+};
+
+exports.deleteCohort = (req, res) => {
+  res.send("add functionality to delete a cohort");
+};
+
+exports.getStudents = (req, res) => {
+  const { cohort } = req.query;
+  res.send(cohorts[cohort]['students']);
+};
+
+exports.createStudent = (req, res) => {
+  res.send("add functionality to create a student");
+};
+
+exports.updateStudent = (req, res) => {
+  res.send("add functionality to update a student");
+};
+
+exports.deleteStudent = (req, res) => {
+  res.send("add functionality to delete a student");
+};
