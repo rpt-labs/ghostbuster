729e9db669f79dfda2400dd6bfd4c001f2332cc4
michellelockett <shelldog42@gmail.com>
2018-10-28 14:14:49 -0700

    update query routes in front end

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -1,6 +1,7 @@
+
 const { Api } = require('./Api');
-const graphqlUrl = process.env.GRAPHQL_URL || '/graphql';
-const expressUrl = process.env.EXPRESS_URL || 'http://localhost:1234/ghostbuster';
+const graphqlUrl =  '/graphql';
+const expressUrl = '/ghostbuster';
 
 const getCohortsQuery = '{cohorts{name phase students{firstName lastName github}}}';
 
