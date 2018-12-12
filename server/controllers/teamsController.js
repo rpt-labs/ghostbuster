51599c5c841fc1d251e44a3643b36c58fc792bb9
debalina <mukherjee.debalina@gmail.com>
2018-12-12 15:07:19 -0800

    (refactor) update naming convention in teams controller and helper functions

--- server/controllers/teamsController.js
+++ server/controllers/teamsController.js
@@ -1,92 +1,98 @@
-//github project checking utils
+// github project checking utils
 const ghostBustWeek = require('../helpers/teamGhostbusterWeek');
 const ghostBustLifetime = require('../helpers/teamGhostbusterLifetime');
 
-//db
+// db
 const teams = require('../../db/models/teams');
 
-//TEAMS requests TODO: delete functionality
-exports.getTeams = async(req, res) => {
-  const { cohort_id } = req.query;
-  let studentData = cohort_id
-    ? await teams.getTeamsByCohort(cohort_id)
+// TEAMS requests TODO: delete functionality
+exports.getTeams = async (req, res) => {
+  const { cohortId } = req.query;
+  const studentData = cohortId
+    ? await teams.getTeamsByCohort(cohortId)
     : await teams.getAllTeams();
 
   if (studentData.length) {
-    res.status(200).json({students: studentData});
+    res.status(200).json({ students: studentData });
   } else {
-    res.status(400).json({error: "error retrieving students.  check that cohort_id is a valid cohort id"});
+    res.status(400).json({ error: 'error retrieving students. Check that cohort_id is a valid cohort id' });
   }
 };
 
-exports.createTeam = async(req, res) => {
-  const { team_name, team_type, github, cohort_id } = req.query;
-  let team = await teams.addTeam({ team_name, team_type, github}, cohort_id);
+exports.createTeam = async (req, res) => {
+  const {
+    teamName, teamType, github, cohortId,
+  } = req.query;
+  const team = await teams.addTeam({
+    teamName, teamType, github, cohortId,
+  });
 
-  if (team.name==="error") {
-    res.status(400).json({error: team.detail || team});
+  if (team.name === 'error') {
+    res.status(400).json({ error: team.detail || team });
   } else {
-    res.status(200).json({team});
+    res.status(200).json({ team });
   }
 };
 
-exports.updateTeam = async(req, res) => {
-  const { teamId, team_name, team_type, github, cohort_id } = req.query;
-  let updated = await teams.updateTeam(teamId, {
-    team_name,
-    team_type,
+exports.updateTeam = async (req, res) => {
+  const {
+    teamId, teamName, teamType, github, cohortId,
+  } = req.query;
+  const updated = await teams.updateTeam(teamId, {
+    teamName,
+    teamType,
     github,
-    cohort_id
+    cohortId,
   });
 
   if (updated.name === 'error') {
-    res.status(200).json({error: "error updating student"});
+    res.status(200).json({ error: 'error updating student' });
   } else {
-    res.status(200).json({student: updated});
+    res.status(200).json({ student: updated });
   }
 };
 
 exports.deleteTeam = (req, res) => {
-  res.send("add functionality to delete a team");
+  res.send('add functionality to delete a team');
 };
 
-//TEAM_STUDENT requests TODO: error handling for all functions
-exports.addStudentsToTeam = async(req, res) => {
+// TEAM_STUDENT requests TODO: error handling for all functions
+exports.addStudentsToTeam = async (req, res) => {
   const { teamId, studentId } = req.params;
-  let added = await teams.addStudentToTeam(studentId, teamId);
-  res.json({message: added});
+  const added = await teams.addStudentToTeam(studentId, teamId);
+  res.json({ message: added });
 };
 
-exports.removeStudentFromTeam = async(req, res) => {
+exports.removeStudentFromTeam = async (req, res) => {
   const { teamId, studentId } = req.params;
-  let removed = await teams.removeStudentFromTeam(studentId, teamId);
-  res.json({message: removed});
+  const removed = await teams.removeStudentFromTeam(studentId, teamId);
+  res.json({ message: removed });
 };
 
-exports.getTeamStudents = async(req, res) => {
+exports.getTeamStudents = async (req, res) => {
   const { teamId } = req.params;
   const students = await teams.getStudentsByTeamId(teamId);
 
   if (students.name === 'error') {
-    res.status(400).json({error: students.detail || students});
+    res.status(400).json({ error: students.detail || students });
   } else {
     res.status(200).json({ students });
   }
 };
 
-//check last week's project phase
+// check last week's project phase
 
-//TODO: create redis store.  first check redis store to see if that week's data is there
-//only call ghostBust function if it's not in redis, it takes ~40 seconds to run!
-exports.getTeamGithubData = async(req, res, next) => {
-  let { cohort } = req.params;
-  let report = await ghostBustWeek(cohort);
+// TODO: create redis store.  first check redis store to see if that week's data is there
+// only call ghostBust function if it's not in redis, it takes ~40 seconds to run!
+exports.getTeamGithubData = async (req, res) => {
+  const { cohort } = req.params;
+  const report = await ghostBustWeek(cohort);
   res.send(report);
-}
+};
 
-//check lifetime contribution by cohort.
-exports.getLifetimeContributionData = async(req, res, next) => {
+// check lifetime contribution by cohort.
+exports.getLifetimeContributionData = async (req, res) => {
   const { cohort, teamType } = req.params;
-  let report = await ghostBustLifetime(teamType, cohort);
+  const report = await ghostBustLifetime(teamType, cohort);
   res.send(report);
 };
