9692bb6b5c038613961fceff682490d98f99d0d6
debalina <mukherjee.debalina@gmail.com>
2019-01-18 05:11:26 -0800

    refactor GhostbusterButton component to use semantic-ui-react

--- client/src/components/GhostbusterButton.jsx
+++ client/src/components/GhostbusterButton.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  Button, Image, Header, Segment,
+} from 'semantic-ui-react';
 
 const GhostbusterButton = (props) => {
   const { clickHandler } = props;
 
   return (
-    <div id="ghostbuster-button" className="ui center aligned segment">
-      <h2 className="ui header">
-        <img alt="cute_ghost" className="ui image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8J9vA9QPiEykz90ij9IRaR3tMKt_kqA0Xpoe0Ha35owi_bWLa" />
-        <div className="content ghostbuster-button-text">
+    <Segment style={{
+      borderColor: '#F44D63', borderWidth: '1px', marginTop: '30px', marginBottom: '30px',
+    }}
+    >
+      <Header as="h2" textAlign="center">
+        <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8J9vA9QPiEykz90ij9IRaR3tMKt_kqA0Xpoe0Ha35owi_bWLa" />
+        <Header.Content>
           Who you gonna call?
-        </div>
-        <div
-          className="ui button primary"
-          id="getSprints"
-          role="presentation"
-          onClick={e => clickHandler(e)}
-        >
-        GHOSTBUSTER
-        </div>
-      </h2>
-    </div>
+        </Header.Content>
+        <Button primary onClick={e => clickHandler(e)} style={{ marginLeft: '30px' }}>GHOSTBUSTER</Button>
+      </Header>
+    </Segment>
   );
 };
 
