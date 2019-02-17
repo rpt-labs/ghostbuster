e9864be6dfdd2e966c227bd3d2adceb1aef436c1
debalina <mukherjee.debalina@gmail.com>
2019-02-16 21:05:37 -0800

    add styling to checkbox

--- client/src/components/attendance/CheckboxList.jsx
+++ client/src/components/attendance/CheckboxList.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'semantic-ui-react';
+import { Button, Grid } from 'semantic-ui-react';
 import Checkbox from './Checkbox';
+import { PaddedGrid } from '../Styles/TeamStyles';
 
 const CheckboxList = (props) => {
   const { cohorts } = props;
+  const cohortsList = cohorts.map(cohort => (
+    <Grid.Column width={4} key={cohort.name}>
+      <Checkbox
+        cohort={cohort}
+      />
+    </Grid.Column>
+  ));
+
   return (
     <div>
-      {
-        cohorts.map(cohort => (
-          <Checkbox
-            key={cohort.name}
-            cohort={cohort}
-          />))
-      }
-      <br />
+
+      <PaddedGrid columns={4} padded="vertically">
+        {cohortsList}
+        <br />
+      </PaddedGrid>
       <Button primary>SHOW ATTENDANCE</Button>
     </div>
   );
