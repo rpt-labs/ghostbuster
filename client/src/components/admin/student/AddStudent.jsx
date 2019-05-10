d53510d31d71d69a0455f8b3e31e2a10d75f236c
debalina <mukherjee.debalina@gmail.com>
2019-05-10 05:09:23 -0700

    add additional fields to add students form

--- client/src/components/admin/student/AddStudent.jsx
+++ client/src/components/admin/student/AddStudent.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 import { Button, Form } from 'semantic-ui-react';
 
 const options = [
-  { key: '4', text: 'RPT11', value: 'RPT11' },
-  { key: '5', text: 'RPT12', value: 'RPT12' },
-  { key: '6', text: 'RPT13', value: 'RPT13' },
-  { key: '7', text: 'RPT14', value: 'RPT14' },
-  { key: '8', text: 'RPT15', value: 'RPT15' }
+  { key: 1, text: 'RPT11', value: 'RPT11' },
+  { key: 2, text: 'RPT12', value: 'RPT12' },
+  { key: 3, text: 'RPT13', value: 'RPT13' },
+  { key: 4, text: 'RPT14', value: 'RPT14' },
+  { key: 5, text: 'RPT15', value: 'RPT15' }
+];
+
+const enrollmentStatus = [
+  { key: 1, text: 'Enrolled', value: 'enrolled' },
+  { key: 2, text: 'Withdrawn', value: 'withdrawn' },
+  { key: 3, text: 'Removed', value: 'Removed' },
+  { key: 4, text: 'Mulligan', value: 'Mulligan' },
+  { key: 5, text: 'Alum', value: 'Alum' },
+  { key: 6, text: 'Precourse', value: 'Precourse' }
 ];
 
 const AddStudent = () => (
@@ -29,7 +38,18 @@ const AddStudent = () => (
         <input type="text" id="lastName" placeholder="Github Handle" />
       </label>
     </Form.Field>
+    <Form.Field>
+      <label htmlFor="zoomName">
+        Zoom Name
+        <input type="text" id="lastName" placeholder="Zoom Name" />
+      </label>
+    </Form.Field>
     <Form.Select label="Cohort" options={options} placeholder="Select Cohort" />
+    <Form.Select
+      label="Enrollment Status"
+      options={enrollmentStatus}
+      placeholder="Enrollment Status"
+    />
     <Button type="submit">Submit</Button>
   </Form>
 );
