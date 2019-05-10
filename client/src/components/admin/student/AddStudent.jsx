512ec104ff2d56011e668694465a7256d25746a2
debalina <mukherjee.debalina@gmail.com>
2019-05-10 09:08:23 -0700

    refactor AddStudent component

--- client/src/components/admin/student/AddStudent.jsx
+++ client/src/components/admin/student/AddStudent.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Button, Form } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Button, Form, Header } from 'semantic-ui-react';
 
 const options = [
   { key: 1, text: 'RPT11', value: 'RPT11' },
@@ -12,46 +12,54 @@ const options = [
 const enrollmentStatus = [
   { key: 1, text: 'Enrolled', value: 'enrolled' },
   { key: 2, text: 'Withdrawn', value: 'withdrawn' },
-  { key: 3, text: 'Removed', value: 'Removed' },
-  { key: 4, text: 'Mulligan', value: 'Mulligan' },
-  { key: 5, text: 'Alum', value: 'Alum' },
-  { key: 6, text: 'Precourse', value: 'Precourse' }
+  { key: 3, text: 'Removed', value: 'removed' },
+  { key: 4, text: 'Mulligan', value: 'mulligan' },
+  { key: 5, text: 'Alum', value: 'alum' },
+  { key: 6, text: 'Precourse', value: 'precourse' }
 ];
 
-const AddStudent = () => (
-  <Form>
-    <Form.Field>
-      <label htmlFor="firstName">
-        First Name
-        <input type="text" id="firstName" placeholder="First Name" />
-      </label>
-    </Form.Field>
-    <Form.Field>
-      <label htmlFor="lastName">
-        Last Name
-        <input type="text" id="lastName" placeholder="Last Name" />
-      </label>
-    </Form.Field>
-    <Form.Field>
-      <label htmlFor="githubHandle">
-        Github Handle
-        <input type="text" id="lastName" placeholder="Github Handle" />
-      </label>
-    </Form.Field>
-    <Form.Field>
-      <label htmlFor="zoomName">
-        Zoom Name
-        <input type="text" id="lastName" placeholder="Zoom Name" />
-      </label>
-    </Form.Field>
-    <Form.Select label="Cohort" options={options} placeholder="Select Cohort" />
-    <Form.Select
-      label="Enrollment Status"
-      options={enrollmentStatus}
-      placeholder="Enrollment Status"
-    />
-    <Button type="submit">Submit</Button>
-  </Form>
-);
-
-export default AddStudent;
+// eslint-disable-next-line react/prefer-stateless-function
+export default class AddStudent extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header as="h1" textAlign="center">
+          Add Student
+        </Header>
+        <Form>
+          <Form.Field>
+            <label htmlFor="firstName">
+              First Name
+              <input type="text" id="firstName" placeholder="First Name" />
+            </label>
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor="lastName">
+              Last Name
+              <input type="text" id="lastName" placeholder="Last Name" />
+            </label>
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor="githubHandle">
+              Github Handle
+              <input type="text" id="lastName" placeholder="Github Handle" />
+            </label>
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor="zoomName">
+              Zoom Name
+              <input type="text" id="lastName" placeholder="Zoom Name" />
+            </label>
+          </Form.Field>
+          <Form.Select label="Cohort" options={options} placeholder="Select Cohort" />
+          <Form.Select
+            label="Enrollment Status"
+            options={enrollmentStatus}
+            placeholder="Enrollment Status"
+          />
+          <Button type="submit">Submit</Button>
+        </Form>
+      </React.Fragment>
+    );
+  }
+}
