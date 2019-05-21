32790b655faf4dc5cf3a68f8efc97b82247b1b12
debalina <mukherjee.debalina@gmail.com>
2019-05-20 20:15:37 -0700

    add placeholder buttons for cohorts

--- client/src/components/toyProblems/ToyProblems.jsx
+++ client/src/components/toyProblems/ToyProblems.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+const COHORTS = ['RPT13', 'RPT14', 'RPT15'];
 
 function ToyProblems() {
-  return <div>Toy Problems</div>;
+  return (
+    <div>
+      {COHORTS.map(cohort => {
+        return (
+          <Button primary key={cohort}>
+            {cohort}
+          </Button>
+        );
+      })}
+    </div>
+  );
 }
 
 export default ToyProblems;
