24a2b3e45375072ad8850c4ae850006857ffb11f
debalina <mukherjee.debalina@gmail.com>
2019-06-12 18:22:33 -0700

    add teams componet

---
+++ client/src/components/admin/Teams.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Teams() {
+  return <div>teams</div>;
+}
+
+export default Teams;
--- client/src/components/admin/TopPanel.jsx
+++ client/src/components/admin/TopPanel.jsx
@@ -4,10 +4,12 @@ import { Menu } from 'semantic-ui-react';
 import StudentsView from './student/StudentsView';
 import Cohorts from './Cohorts';
 import Sprints from './Sprints';
+import Teams from './Teams';
 
 const RenderedContent = ({ tabName = 'Students' }) => {
   if (tabName === 'Students') return <StudentsView />;
   if (tabName === 'Cohorts') return <Cohorts />;
+  if (tabName === 'Teams') return <Teams />;
   return <Sprints />;
 };
 
@@ -43,6 +45,7 @@ class TopPanel extends Component {
             active={activeItem === 'Sprints'}
             onClick={this.handleItemClick}
           />
+          <Menu.Item name="Teams" active={activeItem === 'Teams'} onClick={this.handleItemClick} />
         </Menu>
         <RenderedContent tabName={activeItem} />
       </div>
