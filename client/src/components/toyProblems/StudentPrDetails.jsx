b9bd0c051b25b757479b805a21196d6428f7390a
debalina <mukherjee.debalina@gmail.com>
2019-06-18 23:30:06 -0700

    add error handling

--- client/src/components/toyProblems/StudentPrDetails.jsx
+++ client/src/components/toyProblems/StudentPrDetails.jsx
@@ -23,56 +23,67 @@ export default class StudentPrDetails extends Component {
     return (
       <div>
         <br />
-        <Header as="h1">
-          Selected Cohort: {selectedCohort.toUpperCase()}
-          <Button
-            color="grey"
-            style={{ float: 'right', marginRight: '0px' }}
-            onClick={() => this.showHideDetails()}
-          >
-            {showAllAttemptedToyProblems ? 'Hide Details' : 'Show Details'}
-          </Button>
-        </Header>
-
-        <Card.Group itemsPerRow={2}>
-          {pullRequestsList.map(item => (
-            <Card>
-              <Card.Content>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={`https://github.com/hackreactor/${
-                    item.cohort
-                  }-toy-problems/pulls?q=is:pr+author:${item.studentGithubHandle}`}
-                >
-                  <Card.Header style={{ marginBottom: '10px' }}>
-                    <Label size="big" color="teal">
-                      <Icon name="github" />
-                      {item.studentName}
-                    </Label>
-                    <Label size="big" color="teal" style={{ float: 'right', marginRight: '30px' }}>
-                      <Icon name="calculator" />
-                      {item.uniqueMatchedPrCount}
-                    </Label>
-                  </Card.Header>
-                </a>
-                <Card.Description />
-                {showAllAttemptedToyProblems ? (
-                  <div>
-                    {item.matchedPrs && item.matchedPrs.length ? (
-                      item.matchedPrs.map(pr => <div>{pr}</div>)
+        {pullRequestsList && pullRequestsList.length ? (
+          <div>
+            <Header as="h1">
+              Selected Cohort: {selectedCohort.toUpperCase()}
+              <Button
+                color="grey"
+                style={{ float: 'right', marginRight: '0px' }}
+                onClick={() => this.showHideDetails()}
+              >
+                {showAllAttemptedToyProblems ? 'Hide Details' : 'Show Details'}
+              </Button>
+            </Header>
+            <Card.Group itemsPerRow={2}>
+              {pullRequestsList.map(item => (
+                <Card>
+                  <Card.Content>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href={`https://github.com/hackreactor/${
+                        item.cohort
+                      }-toy-problems/pulls?q=is:pr+author:${item.studentGithubHandle}`}
+                    >
+                      <Card.Header style={{ marginBottom: '10px' }}>
+                        <Label size="big" color="teal">
+                          <Icon name="github" />
+                          {item.studentName}
+                        </Label>
+                        <Label
+                          size="big"
+                          color="teal"
+                          style={{ float: 'right', marginRight: '30px' }}
+                        >
+                          <Icon name="calculator" />
+                          {item.uniqueMatchedPrCount}
+                        </Label>
+                      </Card.Header>
+                    </a>
+                    <Card.Description />
+                    {showAllAttemptedToyProblems ? (
+                      <div>
+                        {item.matchedPrs && item.matchedPrs.length ? (
+                          item.matchedPrs.map(pr => <div>{pr}</div>)
+                        ) : (
+                          <div />
+                        )}
+                      </div>
                     ) : (
                       <div />
                     )}
-                  </div>
-                ) : (
-                  <div />
-                )}
-                <br />
-              </Card.Content>
-            </Card>
-          ))}
-        </Card.Group>
+                    <br />
+                  </Card.Content>
+                </Card>
+              ))}
+            </Card.Group>
+          </div>
+        ) : (
+          <div style={{ margin: '30px', fontSize: '40px', fontWeight: 'bold' }}>
+            {`No Pull Request found for ${selectedCohort.toUpperCase()}`}
+          </div>
+        )}
       </div>
     );
   }
