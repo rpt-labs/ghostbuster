c2232959d81363753e10e29b9268bf6a9ce63dcf
debalina <mukherjee.debalina@gmail.com>
2019-06-24 23:00:58 -0700

    add capability to select a cohort in add teams flow

--- client/src/components/admin/teams/CreateTeams.jsx
+++ client/src/components/admin/teams/CreateTeams.jsx
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
+import RadioButtonList from '../../shared/RadioButtonList';
+import { getAllCohorts } from '../../../queries/queries';
 
-function CreateTeams() {
-  return <div>Create Teams</div>;
+class CreateTeams extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cohorts: []
+    };
+    this.handleRadioButtonChange = this.handleRadioButtonChange.bind(this);
+    this.getCohortsList = this.getCohortsList.bind(this);
+  }
+
+  componentDidMount() {
+    this.getCohortsList();
+  }
+
+  getCohortsList() {
+    getAllCohorts().then(result => {
+      const cohortsList = result.data.data.cohorts.map(e => e.name.toUpperCase());
+      this.setState({
+        cohorts: cohortsList.map(e => ({
+          name: e,
+          isChecked: false
+        }))
+      });
+    });
+  }
+
+  handleRadioButtonChange(cohort) {
+    const { cohorts } = this.state;
+    const newCohortList = cohorts.slice();
+    newCohortList.forEach(e => {
+      if (e.name === cohort) {
+        e.isChecked = true;
+      } else {
+        e.isChecked = false;
+      }
+    });
+    this.setState({ cohorts: newCohortList });
+  }
+
+  showDetails() {
+    console.log('TODO: display list of students for the selected cohort');
+  }
+
+  render() {
+    const { cohorts } = this.state;
+
+    return (
+      <React.Fragment>
+        <RadioButtonList
+          cohorts={cohorts}
+          handleRadioButtonChange={this.handleRadioButtonChange}
+          showDetails={this.showDetails}
+          buttonLabel="SHOW DETAILS"
+        />
+      </React.Fragment>
+    );
+  }
 }
 
 export default CreateTeams;
