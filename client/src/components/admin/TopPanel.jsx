44446830dc5bcdf6417754ef03c1215a5ab5fadc
debalina <mukherjee.debalina@gmail.com>
2019-07-14 22:45:05 -0700

    get cohorts list from the db instead of using hard coded values

--- client/src/components/admin/TopPanel.jsx
+++ client/src/components/admin/TopPanel.jsx
@@ -5,23 +5,47 @@ import StudentsView from './student/StudentsView';
 import Cohorts from './Cohorts';
 import Sprints from './Sprints';
 import TeamsView from './teams/TeamsView';
+import { getAllCohorts } from '../../queries/queries';
 
-const RenderedContent = ({ tabName = 'Students' }) => {
-  if (tabName === 'Students') return <StudentsView />;
+const RenderedContent = ({ tabName = 'Students', cohorts }) => {
+  if (tabName === 'Students') return <StudentsView cohorts={cohorts} />;
   if (tabName === 'Cohorts') return <Cohorts />;
   if (tabName === 'Teams') return <TeamsView />;
   return <Sprints />;
 };
 
 RenderedContent.propTypes = {
-  tabName: PropTypes.string.isRequired
+  tabName: PropTypes.string.isRequired,
+  cohorts: PropTypes.instanceOf(Array).isRequired
 };
 
 class TopPanel extends Component {
   constructor(props) {
     super(props);
-    this.state = { activeItem: 'Students' };
+    this.state = {
+      activeItem: 'Students',
+      cohorts: [],
+      studentsListByCohort: []
+    };
     this.handleItemClick = this.handleItemClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.getCohortsList();
+  }
+
+  getCohortsList() {
+    getAllCohorts().then(result => {
+      const studentsListByCohort = result.data.data.cohorts;
+      const cohortsList = result.data.data.cohorts.map(e => e.name.toUpperCase());
+      this.setState({
+        studentsListByCohort,
+        cohorts: cohortsList.map(e => ({
+          name: e,
+          isChecked: false
+        }))
+      });
+    });
   }
 
   handleItemClick(e, { name }) {
@@ -29,8 +53,7 @@ class TopPanel extends Component {
   }
 
   render() {
-    const { activeItem } = this.state;
-
+    const { activeItem, cohorts } = this.state;
     return (
       <div>
         <Menu tabular>
@@ -51,7 +74,7 @@ class TopPanel extends Component {
           />
           <Menu.Item name="Teams" active={activeItem === 'Teams'} onClick={this.handleItemClick} />
         </Menu>
-        <RenderedContent tabName={activeItem} />
+        <RenderedContent tabName={activeItem} cohorts={cohorts} />
       </div>
     );
   }
