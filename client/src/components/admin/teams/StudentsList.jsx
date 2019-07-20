ff5486018c27842c8114b9ba133f9a395871d4e8
debalina <mukherjee.debalina@gmail.com>
2019-07-20 15:00:16 -0700

    make studentsList a stateful component

--- client/src/components/admin/teams/StudentsList.jsx
+++ client/src/components/admin/teams/StudentsList.jsx
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { List, Checkbox } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import CreateTeamModal from './CreateTeamModal';
 
-function StudentsList(props) {
-  const { currentStudents } = props;
-  return (
-    <React.Fragment>
-      <h2>Enrolled Students</h2>
-      <List>
-        {currentStudents.map(student => (
-          <List.Item key={student.github}>
-            <Checkbox label={`${student.firstName} ${student.lastName}`} />
-          </List.Item>
-        ))}
-      </List>
-      <CreateTeamModal />
-    </React.Fragment>
-  );
+class StudentsList extends Component {
+  constructor(props) {
+    const { currentStudents } = props;
+    super(props);
+    this.state = {
+      studentsList: currentStudents.map(student => Object.assign(student, { isChecked: false }))
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = e => console.log('Click', e.target.innerHTML);
+
+  render() {
+    const { studentsList } = this.state;
+    return (
+      <React.Fragment>
+        <h2>Enrolled Students</h2>
+        <List>
+          {studentsList.map(student => (
+            <List.Item key={student.github}>
+              <Checkbox
+                label={`${student.firstName} ${student.lastName}`}
+                onClick={this.handleClick}
+              />
+            </List.Item>
+          ))}
+        </List>
+        <CreateTeamModal />
+      </React.Fragment>
+    );
+  }
 }
 
 export default StudentsList;
