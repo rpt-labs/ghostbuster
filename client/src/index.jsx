2f5be97d1fd48550c99b03682f3bad85b2a19898
debalina <mukherjee.debalina@gmail.com>
2019-11-18 12:40:29 -0800

    Allow opening the client without auth

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,8 @@ import '@babel/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AlternateApp from './components/AlternateApp';
+import AlternateAppWithoutAuth from './components/AlternateAppWithoutAuth';
 
 require('./styles.css');
 
-ReactDOM.render(<AlternateApp />, document.getElementById('app'));
+ReactDOM.render(<AlternateAppWithoutAuth />, document.getElementById('app'));
