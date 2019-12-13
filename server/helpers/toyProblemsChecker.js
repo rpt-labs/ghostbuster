774aabf2dea6ee1ba32c91ae5db712748b92cbaa
debalina <mukherjee.debalina@gmail.com>
2019-12-14 02:51:00 +0530

    refactor(toy-problems): always get list of released toy problems from github history instead of pre-defined list of toy problems

--- server/helpers/toyProblemsChecker.js
+++ server/helpers/toyProblemsChecker.js
@@ -1,6 +1,5 @@
 const githubQuery = require('./githubQuery');
 const { allCohorts } = require('../config/cohorts');
-const { allToyProblems } = require('../config/toyproblems');
 
 // so node won't throw an error and crash when a student doesn't have a fork
 process.on('uncaughtException', err => {
@@ -12,17 +11,51 @@ const getStudentsList = cohortName => {
   return studentsList && studentsList.length ? studentsList[0].students : [];
 };
 
-const checkIfPrTitleMatches = prTitle => {
-  return allToyProblems.some(substring => prTitle.toLowerCase().includes(substring));
+const getReleasedToyProblems = async cohort => {
+  const response = await githubQuery(
+    `https://api.github.com/repos/hackreactor/${cohort}-toy-problems/commits?per_page=100`
+  );
+
+  let releasedProblems = [];
+  if (response && response.length) {
+    releasedProblems = response
+      .map(res => ({
+        name: res.commit.message,
+        date: res.commit.author.date
+      }))
+      .filter(problem => problem.name.includes('(problem)'))
+      // eslint-disable-next-line no-return-assign
+      .map(problem => ({ name: problem.name.split(' ')[1], date: problem.date }))
+      .sort((a, b) => {
+        return new Date(a.date) - new Date(b.date);
+      });
+  }
+  // check and remove if there are duplicate releases
+  releasedProblems = releasedProblems.reduce((accumulator, current) => {
+    const isAlreadyExist = () => accumulator.some(item => item.name === current.name);
+
+    return isAlreadyExist(current) ? accumulator : [...accumulator, current];
+  }, []);
+  return releasedProblems;
 };
 
-const AllPrsWithMatchingTitles = async studentPrList => {
-  const allmatchedFileNames = studentPrList.filter(e => checkIfPrTitleMatches(e.toLowerCase()));
+const checkIfPrTitleMatches = async (prTitle, cohort) => {
+  const allProblems = await getReleasedToyProblems(cohort);
+  const allReleasedProblemsList = allProblems.map(item => item.name.toLowerCase());
+  return allReleasedProblemsList.some(substring => prTitle.toLowerCase().includes(substring));
+};
+
+const AllPrsWithMatchingTitles = async (studentPrList, cohort) => {
+  const allmatchedFileNames = studentPrList.filter(e =>
+    checkIfPrTitleMatches(e.toLowerCase(), cohort)
+  );
 
   return allmatchedFileNames;
 };
 
-const numberOfUniquePrsWithMatchingTitles = prList => {
+const numberOfUniquePrsWithMatchingTitles = async (prList, cohort) => {
+  const allProblems = await getReleasedToyProblems(cohort);
+  const allReleasedProblemsList = allProblems.map(item => item.name.toLowerCase());
   const allMatchedStrings = prList.map(pr => {
     const prArray = pr
       .toLowerCase()
@@ -30,7 +63,7 @@ const numberOfUniquePrsWithMatchingTitles = prList => {
       .replace(/[^a-z ]/g, '')
       .split(' ');
 
-    return prArray.filter(str => allToyProblems.indexOf(str) > -1);
+    return prArray.filter(str => allReleasedProblemsList.indexOf(str) > -1);
   });
 
   const flattenedMatchedStrings = [];
@@ -67,10 +100,10 @@ const getPrListForStudent = async (cohort, student) => {
       });
       let filesChanged = await getFilesChanged(pullRequestUrls);
       filesChanged = [].concat(...filesChanged);
-      matchedFileNames = (await AllPrsWithMatchingTitles(filesChanged)) || [];
+      matchedFileNames = (await AllPrsWithMatchingTitles(filesChanged, cohort)) || [];
       // remove duplicates
       matchedFileNames = [...new Set(matchedFileNames.map(pr => pr.toLowerCase().trim()))];
-      matchedFilesCount = await numberOfUniquePrsWithMatchingTitles(matchedFileNames);
+      matchedFilesCount = await numberOfUniquePrsWithMatchingTitles(matchedFileNames, cohort);
     }
   } catch (error) {
     console.log(error);
@@ -86,35 +119,6 @@ const checkToyProblems = async cohort => {
   };
   const allPrs = await getAllprs();
   return allPrs;
-};
-
-const getReleasedToyProblems = async cohort => {
-  const response = await githubQuery(
-    `https://api.github.com/repos/hackreactor/${cohort}-toy-problems/commits?per_page=100`
-  );
-
-  let releasedProblems = [];
-  if (response && response.length) {
-    releasedProblems = response
-      .map(res => ({
-        name: res.commit.message,
-        date: res.commit.author.date
-      }))
-      .filter(problem => problem.name.includes('(problem)'))
-      // eslint-disable-next-line no-return-assign
-      .map(problem => ({ name: problem.name.split(' ')[1], date: problem.date }))
-      .sort((a, b) => {
-        return new Date(a.date) - new Date(b.date);
-      });
-  }
-  // check and remove if there are duplicate releases
-  releasedProblems = releasedProblems.reduce((accumulator, current) => {
-    const isAlreadyExist = () => accumulator.some(item => item.name === current.name);
-
-    return isAlreadyExist(current) ? accumulator : [...accumulator, current];
-  }, []);
-
-  return releasedProblems;
 };
 
 module.exports = { checkToyProblems, getReleasedToyProblems };
