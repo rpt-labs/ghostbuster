9fc987f28e466c4f4b84d6f430d373a57cea645d
debalina <mukherjee.debalina@gmail.com>
2020-02-08 12:47:15 -0800

    do not use routes with auth until okta is implemented

--- client/src/components/TopNav.jsx
+++ client/src/components/TopNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withAuth } from '@okta/okta-react';
+// import { withAuth } from '@okta/okta-react';
 import { Menu } from 'semantic-ui-react';
 import { TopNavMenu, StyledLink } from './Styles/TopNavStyles';
 
@@ -23,7 +23,8 @@ const TopNav = props => (
 );
 
 TopNav.propTypes = {
-  auth: PropTypes.instanceOf(Object).isRequired
+  auth: PropTypes.instanceOf(Object)
 };
 
-export default withAuth(TopNav);
+// export default withAuth(TopNav);
+export default TopNav;
