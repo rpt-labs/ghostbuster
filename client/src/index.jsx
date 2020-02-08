66edc334aa128903d0e53937c056272fe99cbdce
debalina <mukherjee.debalina@gmail.com>
2020-02-08 12:33:21 -0800

    refactor to use graphql to get released toy problems

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,6 @@
 import '@babel/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AlternateApp from './components/AlternateApp';
 import AlternateAppWithoutAuth from './components/AlternateAppWithoutAuth';
 
 require('./styles.css');
