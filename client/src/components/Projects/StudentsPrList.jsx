4dc1f3815e9ec0d0ef1935c13bcffb6b9b7b95b0
debalina <mukherjee.debalina@gmail.com>
2020-08-09 23:39:55 -0700

    add students card

--- client/src/components/Projects/StudentsPrList.jsx
+++ client/src/components/Projects/StudentsPrList.jsx
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Label, Card, Icon } from 'semantic-ui-react';
 
 export default class StudentsPrList extends Component {
   render() {
     const { studentsList } = this.props;
     return (
       <div>
-        {studentsList.map(student => (
-          <li key={student.github}>{`${student.firstName} ${student.lastName} - ${student.fecUrls}`}</li>
-        ))}
+        <Card.Group itemsPerRow={2}>
+          {studentsList.map(item => (
+            <Card key={item.github} style={{ marginBottom: '0px' }}>
+              <Card.Content style={{ paddingBottom: '0px' }}>
+                <a target="_blank" rel="noopener noreferrer" href={``}>
+                  <Card.Header style={{ marginBottom: '10px' }}>
+                    <Label size="big">
+                      <Icon name="github" />
+                      {`${item.firstName} ${item.lastName}`}
+                    </Label>
+                  </Card.Header>
+                </a>
+              </Card.Content>
+            </Card>
+          ))}
+        </Card.Group>
       </div>
     );
   }
 }
+
+StudentsPrList.propTypes = {
+  studentsList: PropTypes.instanceOf(Array).isRequired
+};
+
