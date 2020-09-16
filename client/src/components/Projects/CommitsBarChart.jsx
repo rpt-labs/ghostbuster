b1ff388479fac0014bc426028acacd0fc1d523e6
debalina <mukherjee.debalina@gmail.com>
2020-09-16 05:50:50 -0700

    convert to line chart

--- client/src/components/Projects/CommitsBarChart.jsx
+++ client/src/components/Projects/CommitsBarChart.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 import PropTypes from 'prop-types';
 
 // eslint-disable-next-line react/prefer-stateless-function
@@ -22,8 +22,8 @@ export default class CommitsBarChart extends React.Component {
       datasets: [
         {
           label: 'Commits',
-          backgroundColor: 'rgba(75,192,192,1)',
-          borderColor: 'rgba(0,0,0,1)',
+          backgroundColor: 'rgba(75,192,192,0.2)',
+          borderColor: 'rgba(75,192,192,1)',
           borderWidth: 1,
           data: Object.values(numberOfCommitsByDate)
         }
@@ -31,7 +31,7 @@ export default class CommitsBarChart extends React.Component {
     };
     return (
       <div>
-        <Bar
+        <Line
           data={commitData}
           options={{
             title: {
