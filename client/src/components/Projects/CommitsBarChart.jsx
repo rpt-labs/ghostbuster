388b5fe09db02eef89005aa436261fe5ef815d31
debalina <mukherjee.debalina@gmail.com>
2020-09-16 06:46:57 -0700

    initial implementation for showing line chart for entire data range

--- client/src/components/Projects/CommitsBarChart.jsx
+++ client/src/components/Projects/CommitsBarChart.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Line } from 'react-chartjs-2';
 import PropTypes from 'prop-types';
 
-// eslint-disable-next-line react/prefer-stateless-function
 export default class CommitsBarChart extends React.Component {
   getCommitData() {
     const { commits } = this.props;
@@ -11,21 +10,41 @@ export default class CommitsBarChart extends React.Component {
       date: commit.date.split('T')[0]
     }));
     const numberOfCommitsByDate = {};
+
     // eslint-disable-next-line no-return-assign
     commitsWithCalendarDate.forEach(commit =>
       numberOfCommitsByDate[commit.date]
         ? (numberOfCommitsByDate[commit.date] += 1)
         : (numberOfCommitsByDate[commit.date] = 1)
     );
+    const dates = Object.keys(numberOfCommitsByDate);
+    console.log('numberOfCommitsByDate', dates);
+
+    const startDate = new Date(dates[0]);
+    const endDate = new Date(dates[dates.length - 1]);
+    const datesArray = [];
+
+    while (startDate <= endDate) {
+      datesArray.push(startDate.toISOString().split('T')[0]);
+      startDate.setDate(startDate.getDate() + 1);
+    }
+    const commitsForDaysInRange = {};
+    // eslint-disable-next-line no-return-assign
+    datesArray.forEach(date =>
+      !numberOfCommitsByDate[date]
+        ? (commitsForDaysInRange[date] = 0)
+        : (commitsForDaysInRange[date] = numberOfCommitsByDate[date])
+    );
+
     const commitData = {
-      labels: Object.keys(numberOfCommitsByDate),
+      labels: Object.keys(commitsForDaysInRange),
       datasets: [
         {
           label: 'Commits',
           backgroundColor: 'rgba(75,192,192,0.2)',
           borderColor: 'rgba(75,192,192,1)',
-          borderWidth: 1,
-          data: Object.values(numberOfCommitsByDate)
+          borderWidth: 2,
+          data: Object.values(commitsForDaysInRange)
         }
       ]
     };
@@ -52,10 +71,15 @@ export default class CommitsBarChart extends React.Component {
                 ticks: {
                   beginAtZero: true,
                   min: 0,
-                  max: 25
+                  max: 20
                 }
               }
             ]
+          },
+          elements: {
+            point: {
+              radius: 0
+            }
           }
         }}
       />
