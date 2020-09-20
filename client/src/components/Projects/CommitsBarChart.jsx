4da3f1eef3cde40487218bf885b6d6f59f6631a5
debalina <mukherjee.debalina@gmail.com>
2020-09-20 05:45:20 -0700

    display data by week

--- client/src/components/Projects/CommitsBarChart.jsx
+++ client/src/components/Projects/CommitsBarChart.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Line } from 'react-chartjs-2';
 import PropTypes from 'prop-types';
 
+const moment = require('moment');
+
 export default class CommitsBarChart extends React.Component {
   getCommitData() {
     const { commits } = this.props;
@@ -35,6 +37,18 @@ export default class CommitsBarChart extends React.Component {
         : (commitsForDaysInRange[date] = numberOfCommitsByDate[date])
     );
 
+    const sortedByWeek = datesArray.reduce((res, date) => {
+      const count = commitsForDaysInRange[date];
+      const startOfWeek = moment(date, 'YYYY-MM-DD')
+        .startOf('week')
+        .add(1, 'days');
+      // eslint-disable-next-line no-unused-expressions
+      res[startOfWeek]
+        ? (res[startOfWeek].count += count)
+        : (res[startOfWeek] = { date: moment(startOfWeek).format('YYYY-MM-DD'), count });
+      return res;
+    }, {});
+
     const commitData = {
       labels: Object.keys(commitsForDaysInRange),
       datasets: [
@@ -47,17 +61,36 @@ export default class CommitsBarChart extends React.Component {
         }
       ]
     };
-    return commitData;
+
+    const commitDataByWeek = {
+      labels: Object.keys(sortedByWeek).map(date => new Date(date).toISOString().split('T')[0]),
+      datasets: [
+        {
+          label: 'Commits',
+          backgroundColor: 'rgba(75,192,192,0.2)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderWidth: 2,
+          data: Object.values(sortedByWeek).map(value => value.count)
+        }
+      ]
+    };
+
+    return { commitData, commitDataByWeek };
   }
 
   render() {
+    const displayByWeek = true;
+    const { commitData, commitDataByWeek } = this.getCommitData();
+    const data = displayByWeek ? commitDataByWeek : commitData;
+    const max = displayByWeek ? 50 : 20;
+    const text = displayByWeek ? 'Commits by week' : 'Commits by date';
     return (
       <Line
-        data={this.getCommitData()}
+        data={data}
         options={{
           title: {
             display: true,
-            text: 'Commits by date',
+            text,
             fontSize: 20
           },
           legend: {
@@ -70,7 +103,7 @@ export default class CommitsBarChart extends React.Component {
                 ticks: {
                   beginAtZero: true,
                   min: 0,
-                  max: 20
+                  max
                 }
               }
             ]
--- client/src/components/Projects/SelectOptions.jsx
+++ client/src/components/Projects/SelectOptions.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, Radio, Segment } from 'semantic-ui-react';
+import { Form, Radio } from 'semantic-ui-react';
 
 export default class SelectOptions extends Component {
   state = {};
