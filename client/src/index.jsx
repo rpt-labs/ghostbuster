cc2600d017085459a15c031f1a3d775a0ff6df71
Debalina <dmukherjee@users.noreply.github.com>
2021-04-03 11:11:40 -0700

    Update index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,7 @@ import '@babel/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+// import App from './components/AppWithoutAuth';
 
 require('./styles.css');
 
