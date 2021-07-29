8e8758887a0d7a8bf303b8162b86cf1be911002e
debalina <mukherjee.debalina@gmail.com>
2021-07-29 14:53:42 -0700

    refactor to functional component

--- client/src/components/toyProblems/StudentPrDetails.jsx
+++ client/src/components/toyProblems/StudentPrDetails.jsx
@@ -1,130 +1,90 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Label, Card, Icon, Button, Header } from 'semantic-ui-react';
 import ReleasedToyProblems from './ReleasedToyProblems';
 
-export default class StudentPrDetails extends Component {
-  state = {
-    showAllAttemptedToyProblems: false
-  };
+const StudentPrDetails = ({ pullRequestsList, selectedCohort, releasedToyProblems }) => {
+  const [showAllAttemptedToyProblems, setShowAllAttemptedToyProblems] = useState(false);
+  const [showHideReleasedTpList, setShowHideReleasedTpList] = useState(false);
 
-  showHideDetails = () => {
-    const { showAllAttemptedToyProblems } = this.state;
-    this.setState({ showAllAttemptedToyProblems: !showAllAttemptedToyProblems });
-  };
+  const totalReleasedTp = releasedToyProblems.length;
 
-  showHideReleasedList = () => {
-    const { showHideReleasedList } = this.state;
-    this.setState({ showHideReleasedList: !showHideReleasedList });
-  };
-
-  render() {
-    const { pullRequestsList, selectedCohort, releasedToyProblems } = this.props;
-    const { showAllAttemptedToyProblems, showHideReleasedList } = this.state;
-
-    const totalReleasedTp = releasedToyProblems.length;
-
-    return (
-      <div>
-        <br />
-        {pullRequestsList && pullRequestsList.length ? (
-          <div>
-            <Header as="h1">
-              {`Cohort: ${selectedCohort.toUpperCase()}`}
-              <Button
-                color="grey"
-                style={{ float: 'right', marginRight: '0px' }}
-                onClick={() => this.showHideReleasedList()}
-              >
-                {!showHideReleasedList
-                  ? `Released Total: ${totalReleasedTp} - View`
-                  : `Released Total: ${totalReleasedTp} - Hide`}
-              </Button>
-              {showHideReleasedList && (
-                <ReleasedToyProblems releasedToyProblems={releasedToyProblems} />
-              )}
-            </Header>
-            <Header as="h1">
-              <Button color="grey" onClick={() => this.showHideDetails()}>
-                {showAllAttemptedToyProblems ? 'Hide Detailed Report' : 'Show Detailed Report'}
-              </Button>
-              <span
-                style={{
-                  float: 'right',
-                  marginRight: '0px',
-                  fontSize: '14px',
-                  fontWeight: '5',
-                  color: 'red',
-                  fontStyle: 'italic'
-                }}
-              >
-                <b>* Red: </b>
-                Not attempted
-              </span>
-            </Header>
-            <Card.Group itemsPerRow={2}>
-              {pullRequestsList.map(item => (
-                <Card key={item.studentName} style={{ marginBottom: '0px' }}>
-                  <Card.Content style={{ paddingBottom: '0px' }}>
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href={`https://github.com/hackreactor/${
-                        item.cohort
-                      }-toy-problems/pulls?q=is:pr+author:${item.studentGithubHandle}`}
-                    >
-                      <Card.Header style={{ marginBottom: '10px' }}>
-                        <Label size="big" color="teal">
-                          <Icon name="github" />
-                          {item.studentName}
-                        </Label>
-                        <Label
-                          size="big"
-                          color="teal"
-                          style={{ float: 'right', marginRight: '30px' }}
-                        >
-                          <Icon name="check" />
-                          {item.matchedFilesCount}
-                        </Label>
-                      </Card.Header>
-                    </a>
-                    <Card.Description />
-                    {!showAllAttemptedToyProblems && (
-                      <>
-                        <div>
-                          {releasedToyProblems &&
-                            releasedToyProblems.length &&
-                            releasedToyProblems.map(
-                              tp =>
-                                !item.matchedFileNames.includes(tp.name.toLowerCase()) && (
-                                  <div key={tp.name}>
-                                    <span style={{ color: 'red' }}>{tp.name}</span>
-                                    <span
-                                      style={{
-                                        color: 'grey',
-                                        paddingLeft: '5px',
-                                        fontStyle: 'italic'
-                                      }}
-                                    >
-                                      {` (${tp.date.split('T')[0]})`}
-                                    </span>
-                                  </div>
-                                )
-                            )}
-                        </div>
-                      </>
-                    )}
-                    {showAllAttemptedToyProblems && (
-                      <>
-                        <div>
-                          {releasedToyProblems &&
-                            releasedToyProblems.length &&
-                            releasedToyProblems.map(tp =>
-                              item.matchedFileNames.includes(tp.name.toLowerCase()) ? (
-                                <div style={{ color: 'grey' }} key={tp.name}>
-                                  {tp.name}
-                                </div>
-                              ) : (
+  return (
+    <div>
+      <br />
+      {pullRequestsList && pullRequestsList.length ? (
+        <div>
+          <Header as="h1">
+            {`Cohort: ${selectedCohort.toUpperCase()}`}
+            <Button
+              color="grey"
+              style={{ float: 'right', marginRight: '0px' }}
+              onClick={() => setShowHideReleasedTpList(!showHideReleasedTpList)}
+            >
+              {!showHideReleasedTpList
+                ? `Released Total: ${totalReleasedTp} - View`
+                : `Released Total: ${totalReleasedTp} - Hide`}
+            </Button>
+            {showHideReleasedTpList && (
+              <ReleasedToyProblems releasedToyProblems={releasedToyProblems} />
+            )}
+          </Header>
+          <Header as="h1">
+            <Button
+              color="grey"
+              onClick={() => setShowAllAttemptedToyProblems(!showAllAttemptedToyProblems)}
+            >
+              {showAllAttemptedToyProblems ? 'Hide Detailed Report' : 'Show Detailed Report'}
+            </Button>
+            <span
+              style={{
+                float: 'right',
+                marginRight: '0px',
+                fontSize: '14px',
+                fontWeight: '5',
+                color: 'red',
+                fontStyle: 'italic'
+              }}
+            >
+              <b>* Red: </b>
+              Not attempted
+            </span>
+          </Header>
+          <Card.Group itemsPerRow={2}>
+            {pullRequestsList.map(item => (
+              <Card key={item.studentName} style={{ marginBottom: '0px' }}>
+                <Card.Content style={{ paddingBottom: '0px' }}>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={`https://github.com/hackreactor/${
+                      item.cohort
+                    }-toy-problems/pulls?q=is:pr+author:${item.studentGithubHandle}`}
+                  >
+                    <Card.Header style={{ marginBottom: '10px' }}>
+                      <Label size="big" color="teal">
+                        <Icon name="github" />
+                        {item.studentName}
+                      </Label>
+                      <Label
+                        size="big"
+                        color="teal"
+                        style={{ float: 'right', marginRight: '30px' }}
+                      >
+                        <Icon name="check" />
+                        {item.matchedFilesCount}
+                      </Label>
+                    </Card.Header>
+                  </a>
+                  <Card.Description />
+                  {!showAllAttemptedToyProblems && (
+                    <>
+                      <div>
+                        {releasedToyProblems &&
+                          releasedToyProblems.length &&
+                          releasedToyProblems.map(
+                            tp =>
+                              !item.matchedFileNames.includes(tp.name.toLowerCase()) && (
                                 <div key={tp.name}>
                                   <span style={{ color: 'red' }}>{tp.name}</span>
                                   <span
@@ -138,25 +98,54 @@ export default class StudentPrDetails extends Component {
                                   </span>
                                 </div>
                               )
-                            )}
-                        </div>
-                      </>
-                    )}
-                    <br />
-                  </Card.Content>
-                </Card>
-              ))}
-            </Card.Group>
-          </div>
-        ) : (
-          <div style={{ margin: '30px', fontSize: '40px', fontWeight: 'bold' }}>
-            {`No Pull Request found for ${selectedCohort.toUpperCase()}`}
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+                          )}
+                      </div>
+                    </>
+                  )}
+                  {showAllAttemptedToyProblems && (
+                    <>
+                      <div>
+                        {releasedToyProblems &&
+                          releasedToyProblems.length &&
+                          releasedToyProblems.map(tp =>
+                            item.matchedFileNames.includes(tp.name.toLowerCase()) ? (
+                              <div style={{ color: 'grey' }} key={tp.name}>
+                                {tp.name}
+                              </div>
+                            ) : (
+                              <div key={tp.name}>
+                                <span style={{ color: 'red' }}>{tp.name}</span>
+                                <span
+                                  style={{
+                                    color: 'grey',
+                                    paddingLeft: '5px',
+                                    fontStyle: 'italic'
+                                  }}
+                                >
+                                  {` (${tp.date.split('T')[0]})`}
+                                </span>
+                              </div>
+                            )
+                          )}
+                      </div>
+                    </>
+                  )}
+                  <br />
+                </Card.Content>
+              </Card>
+            ))}
+          </Card.Group>
+        </div>
+      ) : (
+        <div style={{ margin: '30px', fontSize: '40px', fontWeight: 'bold' }}>
+          {`No Pull Request found for ${selectedCohort.toUpperCase()}`}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default StudentPrDetails;
 
 StudentPrDetails.propTypes = {
   pullRequestsList: PropTypes.instanceOf(Array).isRequired,
